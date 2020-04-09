2d74fbd7a2521c86ed674b910a25223315ee43e9
David Duong <dduong@csumb.edu>
2020-04-08 21:40:05 -0700

    Cell contents now viewable. Constraints set.

--- fxHabit/TaskViewController.swift
+++ fxHabit/TaskViewController.swift
@@ -9,12 +9,37 @@
 import UIKit
 import Parse
 
-class TaskViewController: UIViewController {
+class TaskViewController: UIViewController, UITableViewDelegate, UITableViewDataSource{
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var posts = [PFObject]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
         // Do any additional setup after loading the view.
+    }
+        
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        //Move all into a function gettingposts. Change Classname to posts
+        let query = PFQuery(className:"Tasks")
+        query.includeKeys(["author"])
+        query.limit = 15
+        
+        query.findObjectsInBackground { (posts, error) in
+            if posts != nil {
+                self.posts = posts!
+                self.tableView.reloadData()
+            } else {
+                print("Error, can't load posts")
+            }
+        }
     }
     
     @IBAction func onLogoutButton(_ sender: Any) {
@@ -25,5 +50,18 @@ class TaskViewController: UIViewController {
         
         let delegate = self.view.window?.windowScene?.delegate as! SceneDelegate
         delegate.window!.rootViewController = loginViewController
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return posts.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TaskTableViewCell") as! TaskTableViewCell
+        let post = posts[indexPath.row]
+        cell.titleLabel.text = post["title"] as? String
+        cell.descriptionLabel.text = post["description"] as? String
+        
+        return cell
     }
 }
