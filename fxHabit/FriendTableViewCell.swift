693846131eb6fb9b826ed74f6202ef79e38a332c
Paul Whipp <pwhipp@csumb.edu>
2020-04-21 12:19:18 -0700

    Friends Files and rough frames

---
+++ fxHabit/FriendTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  FriendTableViewCell.swift
+//  fxHabit
+//
+//  Created by user163799 on 4/21/20.
+//  Copyright © 2020 gsidhu.ycarrillo.dduong.pwhipp. All rights reserved.
+//
+
+import UIKit
+
+class Friend_TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ fxHabit/FriendTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  FriendTableViewCell.swift
+//  fxHabit
+//
+//  Created by user163799 on 4/21/20.
+//  Copyright © 2020 gsidhu.ycarrillo.dduong.pwhipp. All rights reserved.
+//
+
+import UIKit
+
+class FriendTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
